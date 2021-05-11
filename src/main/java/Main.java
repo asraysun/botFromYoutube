db3a6f584b77d3641a7cb40c39031f7ed4e30c2b
andrey zapolskiy <uniqo@yandex.by>
2021-05-11 22:05:07 +0300

    add spark to pom.xml and handle for index.html

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,14 @@ import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
+import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
+
+        port(getHerokuAssignedPort());
+        get("/", (req, res) -> "Hello heroku world");
+
 
         ApiContextInitializer.init();
         MyTestBot myTestBot = new MyTestBot();
@@ -16,5 +21,13 @@ public class Main {
         } catch (TelegramApiRequestException e) {
             e.printStackTrace();
         }
+    }
+
+    private static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
